004223a8f0d8ce24175036b46197c4f1e95c590b
DavidTurkGT <dlturk747@gmail.com>
2017-07-26 14:38:01 -0400

    Changed code block for one-line for loops

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         //Create 15 first graders and add them to an ArrayList<Student>
         ArrayList<Student> students = new ArrayList<>();
-        for(int i = 0; i < 15; i++) students.add(new Student("student"+i,"lastname"+i,1));
+        for(int i = 0; i < 15; i++) { students.add(new Student("student"+i,"lastname"+i,1)); }
         //Create 3 teacher and add them to an ArrayList<Teacher>
         ArrayList<Teacher> teachers = new ArrayList<>();
-        for(int i = 0; i < 3; i++) teachers.add(new Teacher("teacher"+i,"lastname"+i,1));
+        for(int i = 0; i < 3; i++) { teachers.add(new Teacher("teacher"+i,"lastname"+i,1)); }
         Map<Teacher,Set<Student>> classes = new HashMap<>();
         for(int i = 0; i < 3; i++){
             Set<Student> classMembers = new HashSet<>();
